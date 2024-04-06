874292390603e030f1e10bd2b69a57f44ae3fa10
rksvw <ritik.vw98@gmail.com>
2024-04-06 12:28:46 +0530

    change done

---
+++ gblog/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../imgs/logo.png';
+
+function Navbar() {
+  return (
+    <nav className=''>
+        <img src={logo}/>
+    </nav>
+  )
+}
+
+export default Navbar
